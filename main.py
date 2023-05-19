b280ac578555c2fab841ae3fd9de8a48bd511c85
Aman <amansingh828355@gmail.com>
2023-05-19 20:56:55 +0530

    commit

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import pyttsx3
-friend = pyttsx3.init()
+readmyapphehe = pyttsx3.init()
 
 print('This is a ReadAI very basic Application in which you can red aloud the text')
 
+
+
 text = input('Enter the text you want to read it aloud:')
 
-friend.say(text)
-friend.runAndWait()
+readmyapphehe.say(text)
+readmyapphehe.runAndWait()
